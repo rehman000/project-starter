9da573c141b24cd158430517a91b467fcfbf23e2
Rehman Arshad <rehman.arshad777@gmail.com>
2019-12-06 19:46:53 -0500

    Update DashboardPage.js

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -9,7 +9,69 @@ import TableDark from '../components/table-dark-component';
 export default class DashboardPage extends Component {
     constructor(props) {
         super(props);
-        this.state = { email: '', password: '' };
+
+        this.deleteItems = this.deleteItems.bind(this)
+
+        this.state = { 
+            Loggedin: true, 
+            AuthToken: "Authentication Token",
+            username: "",
+        };
+
+    }
+
+
+    // Get Token from database store it here, if the user is logged in for too long, 
+    // Reset the Authentication Token on the back end 
+    // If the old token doesn't match with the new one (which it shouldn't) 
+    // Then redirect to sign in page
+
+    componentDidMount() {
+        axios.get('api/items/items', {
+            "headers" : {"authorization" : "bearer " + localStorage.getItem("token")}
+        })
+        .then(response => {
+        this.setState({ items: response.data.items })
+        })
+        .catch((error) => {
+        console.log(error);
+        })
+    }
+
+
+
+    deleteItems(id) {
+    axios.delete('api/items/delete/'+id, {
+        "headers" : {"authorization" : "bearer " + localStorage.getItem("token")}
+    })
+        .then(response => { console.log(response.data)});
+
+    this.setState({
+        items: this.state.items.filter(el => el._id !== id)
+    })
+    }
+
+
+
+    onSubmit(e) {
+        console.log("This function has been called! ... ");
+        console.log("===================================");
+
+        e.preventDefault();
+
+        const pantryTable = document.getElementById("Ingredients");        
+        let formData = new FormData(pantryTable);
+        let items = {};
+        for (let key of formData.keys()){
+            items[key] = formData.get(key);
+        }
+        
+        var value = this.value;
+        console.log(value);
+
+        console.log(formData[0]);
+
+>>>>>>> Stashed changes
     }
   
 
@@ -23,6 +85,15 @@ export default class DashboardPage extends Component {
                 <TableLight />      {/* This is the table light component */}
                 <TableDark />       {/* This is the table dark component */}    
                 
+<<<<<<< Updated upstream
+=======
+                <Cards />           {/* This is the Cards component */}         
+                <TableDark />
+                <TableLight />
+               
+                
+
+>>>>>>> Stashed changes
             </div>
         );
     }
